678f6a8ab8bd2c4e157db282b03f78a5c576b119
2011140071b <2011140071b@ed.fuk.kindai.ac.jp>
2022-10-05 08:19:46 +0000

    initial commit

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,23 @@
+class TopController < ApplicationController
+  def main
+    if session[:login_uid] == nil
+      render "login"
+    else
+      render "main"
+    end
+  end
+  
+  def login
+    if params[:uid] == "kindai" && params[:pass] == "sanriko"
+      session[:login_uid] = params[:uid]
+      redirect_to "/"
+    else
+      render "error"
+    end
+  end
+  
+  def logout
+    session[:login_uid] = nil
+    render "login"
+  end
+end
